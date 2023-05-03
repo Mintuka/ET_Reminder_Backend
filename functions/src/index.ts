0e742b0de0c951df3694ff564c0afa06eb6c91cf
mintuka <mintuka2015@gmail.com>
2023-05-03 07:24:31 +0300

    Refactor backend

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,27 +1,16 @@
 "use strict";
 import { sendMessageToClient } from "./sendMessage";
 
-// const express = require('express');
-// const cors = require('cors');
 const functions = require('firebase-functions');
-// const admin = require('firebase-admin');
-
-// const app = express();
-// app.use(cors({ origin: true }));
-
-// admin.initializeApp();
-
-// app.get('/test', function (request: any, response: any) {
-//   response.send({ success: 'Hi' });
-// });
-
-// app.post('/send-message', async (req:any, res:any) => {
-
-//   })
 
 exports.api = functions.https.onCall(async (data:any, context:any) => {
     const response = await sendMessageToClient(data)
     return response
+})
+
+exports.setAppointments = functions.https.onCall(async (data:any, context:any) => {
+  const response = 'Update all docs'
+  return response
 })
 
 exports.createUser = functions.firestore
